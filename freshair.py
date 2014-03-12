a9c3cf964bbe0568d5421aef187b2aeadb5889e2
Tanim Islam <tanim.islam@gmail.com>
2014-03-12 16:39:39 -0700

    Lots of changes to freshair.py. Added in multiprocessing functionality with non-daemonic

--- freshair.py
+++ freshair.py
@@ -2,9 +2,22 @@
 
 import os, sys, glob, numpy, urllib2, mutagen.mp4
 import multiprocessing, time, lxml.etree, subprocess
+import multiprocessing.pool
 sys.path.append('/mnt/software/sources/nprstuff/simple-junk')
 import npr_utils
 from optparse import OptionParser
+
+# magic to get multiprocessing to get processes to be able to start daemons
+class NoDaemonProcess(multiprocessing.Process):
+    # make 'daemon' attribute always return False
+    def _get_daemon(self):
+        return False
+    def _set_daemon(self, value):
+        pass
+    daemon = property(_get_daemon, _set_daemon) 
+
+class MyPool(multiprocessing.pool.Pool):
+    Process = NoDaemonProcess 
 
 def get_freshair_image():
     return urllib2.urlopen('http://media.npr.org/images/podcasts/2013/primary/fresh_air.png').read()
@@ -20,6 +33,52 @@ def _download_file(input_tuple):
     mp3URL, filename = input_tuple
     with open(filename, 'wb') as openfile:
         openfile.write( urllib2.urlopen(mp3URL).read() )
+
+def get_freshair_date_from_name(candidateNPRFreshAirFile):
+    if not os.path.isfile(candidateNPRFreshAirFile):
+        raise ValueError("Error, %s is not a file," % candidateNPRFreshAirFile )
+    if not os.path.basename(candidateNPRFreshAirFile).endswith('.m4a'):
+        raise ValueError("Error, %s does not end in .m4a" % candidateNPRFreshAirFile )
+    if not os.path.basename(candidateNPRFreshAirFile).startswith('NPR.FreshAir.'):
+        raise ValueError("Error, %s is not a valid file" % candidateNPRFreshAirFile )
+    day, mon, year = [ int(tok) for tok in os.path.basename(candidateNPRFreshAirFile).split('.')[2:5] ]
+    return time.strptime('%d-%d-%04d' % ( day, mon, year ),
+                         '%d-%m-%Y' )
+def get_freshair_valid_dates_remaining_tuples(yearnum, inputdir):
+    freshair_files_downloaded = glob.glob( os.path.join(inputdir, 'NPR.FreshAir.*.%04d.m4a' % yearnum ) )
+    dates_downloaded = set( [ get_freshair_date_from_name(filename) for filename in 
+                              freshair_files_downloaded ] )
+    all_order_weekdays = { datetime : (num+1) for (num, datetime) in
+                           enumerate( npr_utils.get_weekday_times_in_year(yearnum) ) }
+    weekdays_left = filter(lambda datetime: datetime < time.localtime(), set( all_order_weekdays . keys() ) - set( dates_downloaded ) )
+    totnum = len(all_order_weekdays.keys() )
+    order_dates_remain = sorted([ ( all_order_weekdays[datetime], totnum, datetime ) for
+                                  datetime in weekdays_left ], key = lambda tup: tup[0] ) 
+    return order_dates_remain
+
+def _process_freshairs_by_year_tuple(input_tuple):
+    outputdir, totnum, verbose, datetimes_order_tuples = input_tuple
+    fa_image = get_freshair_image()
+    for datetime, order in datetimes_order_tuples:
+        time0 = time.time()
+        fname = get_freshair(outputdir, datetime, order_totnum = ( order, totnum),
+                             file_data = fa_image )
+        if verbose:
+            print 'processed %s in %0.3f seconds.' % ( os.path.basename(fname), time.time() - time0 )
+
+def process_all_freshairs_by_year(yearnum, inputdir, verbose = True):
+    order_dates_remain = get_freshair_valid_dates_remaining_tuples( yearnum, inputdir )
+    if len(order_dates_remain) == 0: return
+    totnum = order_dates_remain[0][1]
+    nprocs = multiprocessing.cpu_count() 
+    input_tuples = [ ( inputdir, totnum, verbose, [ ( datetime, order ) for ( order, totnum, datetime) in 
+                                                    order_dates_remain if (order - 1) % nprocs == procno ] ) for
+                     procno in xrange(nprocs) ]
+    time0 = time.time()
+    pool = MyPool(processes = multiprocessing.cpu_count())
+    pool.map(_process_freshairs_by_year_tuple, input_tuples)
+    if verbose:
+        print 'processed all Fresh Air downloads for %04d in %0.3f seconds.' % ( yearnum, time.time() - time0 ) 
 
 def get_freshair(outputdir, datetime_wkday, order_totnum = None,
                  file_data = None):
@@ -55,7 +114,7 @@ def get_freshair(outputdir, datetime_wkday, order_totnum = None,
         mp3url = urllib2.urlopen( m3uurl ).read().strip()
         title_mp3_urls.append( ( title, mp3url ) )
     
-    decdate = time.strftime('%Y.%m.%d', datetime_s)
+    decdate = time.strftime('%d.%m.%Y', datetime_s)
     
     titles, mp3urls = zip(*title_mp3_urls)
     title = time.strftime('%A, %B %d, %Y', datetime_s)
@@ -106,6 +165,7 @@ def get_freshair(outputdir, datetime_wkday, order_totnum = None,
     mp4tags.tags['trkn'] = [ ( order_in_year, tot_in_year ), ]
     mp4tags.tags['covr'] = [ mutagen.mp4.MP4Cover(file_data, mutagen.mp4.MP4Cover.FORMAT_PNG ), ]
     mp4tags.save()
+    return m4afile
 
 if __name__=='__main__':
     parser = OptionParser()
@@ -118,5 +178,5 @@ if __name__=='__main__':
                       help = 'The date, in the form of "January 1, 2014." The default is today\'s date, %s.' %
                       npr_utils.get_datestring( time.localtime() ) )
     opts, args = parser.parse_args()
-    get_freshair( opts.dirname, npr_utils.get_time_from_datestring( opts.date ) )
+    fname = get_freshair( opts.dirname, npr_utils.get_time_from_datestring( opts.date ) )
                   
