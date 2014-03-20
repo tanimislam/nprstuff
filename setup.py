8a078f96abe23a6f9e462ee752efb36c765aeaf1
Tanim Islam <tanim.islam@gmail.com>
2014-03-20 10:43:04 -0700

    Added setup.py, to see if this thing works.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='nprstuff',
+    version='0.1',
+    packages=["mutagen", "lxml", 'magic', ],
+    url='',
+    license='GPL',
+    author='Tanim Islam',
+    author_email='tanim.islam@gmail.com',
+    description='Some utility scripts.'
+)
