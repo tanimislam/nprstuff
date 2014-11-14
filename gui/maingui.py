ec2a66377e299f29e6ceb5553361613702d750c1
Tanim Islam <tanim.islam@gmail.com>
2014-11-14 12:50:46 -0800

    new file:   gui/maingui.py

---
+++ gui/maingui.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import newyorker, nytimes, sys
+
+class MainGui(QTabWidget):
+    def __init__(self):
+        super(MainGui, self).__init__()
+        nyf = newyorker.NewYorkerFrame(mainFrame = False)
+        nyt = nytimes.NYTimesFrame(mainFrame = False)
+        self.setStyleSheet('QTabWidget {background-color: #f4faff; }')
+        self.addTab( nyf, 'New Yorker Printer' )
+        self.addTab( nyt, 'NYTimes Printer' )
+        self.setWindowTitle('Main Printing GUI')
+        self.setCurrentIndex(0)
+        #
+        # Ctrl-Q
+        exitAction = QAction(self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.triggered.connect(qApp.quit)
+        self.addAction(exitAction)
+        #
+        # Move-Right
+        nextTabAction = QAction(self)
+        nextTabAction.setShortcut('Ctrl+Right')
+        nextTabAction.triggered.connect(self.nextTab)
+        self.addAction(nextTabAction)
+        #
+        # Move-Left
+        prevTabAction = QAction(self)
+        prevTabAction.setShortcut('Ctrl+Left')
+        prevTabAction.triggered.connect(self.prevTab)
+        self.addAction(prevTabAction)
+        #
+        # make visible
+        qfm = QFontMetrics( nyf.qf )
+        wdth = int( 70 * qfm.averageCharWidth() * 1.25 )
+        self.resize( wdth, 900)
+        self.setFixedWidth( wdth )
+        self.show()
+        
+    def nextTab(self):
+        cIndx = self.currentIndex()
+        if cIndx == self.count() - 1: nIndx = 0
+        else: nIndx = cIndx + 1
+        self.setCurrentIndex( nIndx )
+    
+    def prevTab(self):
+        cIndx = self.currentIndex()
+        if cIndx == 0: nIndx = self.count() - 1
+        else: nIndx = cIndx - 1
+        self.setCurrentIndex( nIndx )
+
+if __name__=='__main__':
+    qApp = QApplication(sys.argv)
+    mg = MainGui()
+    sys.exit(qApp.exec_() )
