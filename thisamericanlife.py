1428e4abe5bb5cd36bdeaef37f78745a42275436
Tanim Islam <tanim.islam@gmail.com>
2014-12-30 18:27:42 -0500

    modified:   thisamericanlife.py

--- thisamericanlife.py
+++ thisamericanlife.py
@@ -88,4 +88,5 @@ if __name__=='__main__':
                       help = 'Directory into which to download This American Life episodes. Default is %s.' %
                       '/mnt/media/thisamericanlife')
     options, args = parser.parse_args()
-    get_american_life(options.episode, directory=options.directory)
+    direct = os.path.expanduser( options.directory )
+    get_american_life(options.episode, directory=direct)
