f5b0d58c6db70802c1f697ddb740ea343b8bd1e0
Tanim Islam <tanim.islam@gmail.com>
2021-12-11 21:06:07 -0800

    uh oh fix to titleit.py

--- nprstuff/cli/titleit.py
+++ nprstuff/cli/titleit.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 
 def _main( ):
     parser = ArgumentParser( )
-    parser.add_option(
+    parser.add_argument(
         '-t',
         '--title', dest='title', type=str, action='store', default='',
         help = 'This is title title. Default is blank.')
